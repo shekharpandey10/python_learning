bd5878442223ab88e04d90e77a7962a6e6f6b34e
unknown <CHANDRASHEKHARPANDEY.237112381@gehu.ac.in>
2026-05-16 21:52:16 +0530

    add basic arithmetic and type demonstration scripts

---
+++ day-01/basic.py
@@ -0,0 +1,18 @@
+"""
+operator
+"""
+x=3+4
+print(x)
+print(3-4)
+print(3*4)
+print(3**4)
+print(3%4)
+print(3/4)
+print(3//4)
+
+print('shekhar')
+print('pandey')
+COUNTRY='indian'
+print(COUNTRY)
+print('"I am enjoying learning of python"')
+
---
+++ day-01/type.py
@@ -0,0 +1,23 @@
+""" Types"""
+
+x=4.5
+print(type(x))
+print(type(49))
+print(type(4.19))
+
+print(type(4 - 4j))  #complex
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('shekhar'))
+
+
+
+integer=4
+float_num=4.5
+complex_num=5-4j
+string_val='hello'
+bool_val=True
+list_val=[4,5,6,'helo']
+touple_val=('z',5,6)  #can't be change(immutable)
+set_val={11,2,3,4,3,2}  #unique set_val
+empty_set=set()
+print(set_val)
